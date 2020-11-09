f17bb27d06145c2625a67d06a32678bac3a8ac5b
celvineadiputra_ <celvineadiputra2@gmail.com>
2020-11-09 18:11:06 +0700

    add hero

---
+++ src/components/Hero.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col d-flex justify-content-center align-content-center">
+        <div>
+          <h2>
+            <strong>Delicios Food Menu,</strong><br />
+            in your mind
+          </h2>
+          <p>Makanan Lezat sedang menunggu mu di depan</p>
+          <router-link class="btn btn-success btn-border-none" to="/"
+            >Pesan</router-link
+          >
+        </div>
+      </div>
+      <div class="col">
+        <img :src="hero" :alt="hero" width="500px" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import hero from "../../src/assets/image/Hero.svg";
+export default {
+  name: "Hero",
+  data: function () {
+    return {
+      hero: hero,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
